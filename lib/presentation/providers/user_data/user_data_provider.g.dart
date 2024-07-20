a8e773b4bf72f9297738d549575e7899661c17b0
DelvinWillian <delvinwillian28@gmail.com>
2024-07-20 15:59:29 +0700

    create user data provider

---
+++ lib/presentation/providers/user_data/user_data_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_data_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$userDataHash() => r'e6bed71135ee4561b85bc66cd4b266b7866af09a';
+
+/// See also [UserData].
+@ProviderFor(UserData)
+final userDataProvider =
+    AutoDisposeAsyncNotifierProvider<UserData, User?>.internal(
+  UserData.new,
+  name: r'userDataProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$userDataHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$UserData = AutoDisposeAsyncNotifier<User?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
